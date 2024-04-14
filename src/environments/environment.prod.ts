64c6c4de30ae0275dbf01cd05e6ce67e63b77f32
Marlon Nunes <marlonnunes869@gmail.com>
2024-04-13 23:59:50 -0300

    FIX: url keycloak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    keycloakTokenUrl: 'http://l54.86.252.74:8080/realms/car-rental-security/protocol/openid-connect/token',
+    keycloakTokenUrl: 'http://54.86.252.74:8080/realms/car-rental-security/protocol/openid-connect/token',
     keycloakClientId: 'app',
     keycloakGrantType: 'password',
     api: 'http://54.86.252.74:8081'
